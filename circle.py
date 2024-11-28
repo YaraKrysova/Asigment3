3cfb205f9937575dbad24532e3786970c4d4ed41
Iaroslava Krysova <yaroslavakrysova@Iaroslavas-MacBook-Pro.local>
2024-11-27 20:13:56 -0500

    files were created

---
+++ circle.py
@@ -0,0 +1,6 @@
+from shape import *
+class Circle():
+    _radius: 0.0
+
+    def __init__(self, radius):
+       self._radius = radius 
---
+++ shape.py
@@ -0,0 +1,9 @@
+class Shape:
+    s_count = 0
+    _color = ""
+
+    def __init__(self, color):
+        self._color = color
+
+    def addShape(self):
+        pass
