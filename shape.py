911eed3725cbf8825ba8ac83bd850949910a9a6d
Iaroslava Krysova <yaroslavakrysova@Iaroslavas-MacBook-Pro.local>
2024-12-02 09:34:01 -0500

    decided to make shape as parent class and made separate files for each shape

--- shape.py
+++ shape.py
@@ -9,34 +9,47 @@ class Shape:
 
     def addType(self):
         print("(1)Rectangle (2)Triangle (3)Circle (4)Square (5)Pentagon")
-        self.shape = int(input("Add: "))
+        self.shape = int(input("Add: "))    #represents the type of shape
         self._color = input("Color: ")
         self.lenght = float(input("Length: "))       #Overwrite method in child class
         self.widht = float(input("Width: "))
         
         if self.shape == 1:
             print(f"{self._color} Rectangle with length {self.lenght} and width {self.widht} added")
+            self.edges = 4
         
         elif self.shape == 2:
             print(f"{self._color} Triangle with length {self.lenght} and width {self.widht} added")
+            self.edges = 4
         
         elif self.shape == 3:
-            print(f"{self._color} Circle with length {self.lenght} and width {self.widht} added")
+            print(f"{self._color} Circle with ") ########
         
         elif self.shape == 4:
             print(f"{self._color} Square with length {self.lenght} and width {self.widht} added")
+            self.edges = 4
         
         elif self.shape == 5:
             print(f"{self._color} Pentagon with length {self.lenght} and width {self.widht} added")
+            self.edges = 4
         
         
         Shape.s_count += 1
-        self.shapeList.update({self.shape: [self._color , self.lenght, self.widht]})
+        self.shapeList.update({self.shape: [self._color , self.lenght, self.widht]})    #the list should be updated every time when person write new info
 
     
+    def getPerimeter(self):
+        return self.edges*(self.lenght + self.widht)
+    
+    def getArea(self):
+        if self.shape == 1:
+            self.area = self.lenght * self.widht
+
+        
 
     def desription():
         print(f"Collection has {Shape.s_count} shapes:")
+        
 
         
 
